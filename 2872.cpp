b3b001b617e3c7b446f6b18275c0ea2ab151272e
bogdanwebdeveloper <75477064+bogdanwebdeveloper@users.noreply.github.com>
2022-02-04 11:50:05 +0200

    Create 2872.cpp

---
+++ 2872.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    unsigned n, p;
+    cin >> n;
+    cout << 1;
+    for (int i=1;i<=n;i++)
+    {
+        cout << 0;
+    }
+    return 0;
+}
